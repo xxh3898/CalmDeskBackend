61e084729be7b88187c4b98afe11b863c3d82ce8
1suk <jang1suk@sungkyul.ac.kr>
2026-01-27 17:20:20 +0900

    feat:회사 참여 및 회사 코드 검증 구현 #12

--- src/main/java/com/code808/calmdesk/domain/company/repository/CompanyRepository.java
+++ src/main/java/com/code808/calmdesk/domain/company/repository/CompanyRepository.java
@@ -3,7 +3,7 @@ package com.code808.calmdesk.domain.company.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.code808.calmdesk.domain.member.entity.Company;
+import com.code808.calmdesk.domain.company.entity.Company;
 import java.util.Optional;
 
 @Repository
