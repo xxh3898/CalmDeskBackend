6d7d92ffa2477da9e3048d8ad20ef2ea49055d23
1suk <jang1suk@sungkyul.ac.kr>
2026-02-02 20:40:33 +0900

    feat: 관리자 대시보드 부서별 스트레스 조회 및 위험군 조회 #40

---
+++ src/main/java/com/code808/calmdesk/domain/dashboard/service/admin/DashboardService.java
@@ -0,0 +1,11 @@
+package com.code808.calmdesk.domain.dashboard.service.admin;
+
+import com.code808.calmdesk.domain.dashboard.dto.admin.DashboardDto;
+
+import java.util.List;
+
+public interface DashboardService {
+    List<DashboardDto.DepartmentStats> getDepartmentStats(DashboardDto.DashboardRequest request);
+    List<DashboardDto.HighRiskMember> getHighRiskMembers(DashboardDto.DashboardRequest request);
+//    DashboardDto.CompanyStats getCompanyStats(DashboardDto.DashboardRequest request);
+}
