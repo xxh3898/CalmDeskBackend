0e27999852625e6d9ba6dc63eb6b219db7df2a29
junwon <qkrwnsdjs4048@gmail.com>
2026-02-03 09:25:02 +0900

    fix : 크로스오리진 삭제

--- src/main/java/com/code808/calmdesk/domain/mypage/controller/employee/MyPageController.java
+++ src/main/java/com/code808/calmdesk/domain/mypage/controller/employee/MyPageController.java
@@ -13,7 +13,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/mypage")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:5173")
 public class MyPageController {
 
     private final MyPageService myPageService;
