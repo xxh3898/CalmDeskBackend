edb7939f386c62bc664a5a70ef093e6eb513a160
xxh3898 <chiho3898@gmail.com>
2026-02-27 17:30:55 +0900

    fix: 배포 서버 시간대를 한국 시간(KST)으로 하드코딩 설정

--- src/main/java/com/code808/calmdesk/CalmdeskApplication.java
+++ src/main/java/com/code808/calmdesk/CalmdeskApplication.java
@@ -1,19 +1,25 @@
 package com.code808.calmdesk;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import jakarta.annotation.PostConstruct;
 
 @EnableScheduling // 스케줄링 활성화
 @EnableJpaAuditing
 @SpringBootApplication
-
 public class CalmdeskApplication {
+
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CalmdeskApplication.class, args);
     }
-
 }
